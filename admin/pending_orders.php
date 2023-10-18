2fc83114ef752edc885aecff789467d1ca182276
rshrestha007 <roshasnhr61@gmail.com>
2023-10-18 20:19:49 +0545

    updated

--- admin/pending_orders.php
+++ admin/pending_orders.php
@@ -1,32 +1,28 @@
 <?php
- include'header.php';
- SESSION_START();
+include 'header.php';
+SESSION_START();
 
-if(isset($_SESSION['auth']))
-{
-   if($_SESSION['auth']!=1)
-   {
-       header("location:login.php");
-   }
+if (isset($_SESSION['auth'])) {
+  if ($_SESSION['auth'] != 1) {
+    header("location:login.php");
+  }
+} else {
+  header("location:login.php");
 }
-else
-{
-   header("location:login.php");
-}
-include'lib/connection.php';
+include 'lib/connection.php';
 $sql = "SELECT * FROM orders";
-$result = $conn -> query ($sql);
+$result = $conn->query($sql);
 
-if(isset($_POST['update_update_btn'])){
+if (isset($_POST['update_update_btn'])) {
   $update_value = $_POST['update_status'];
   $update_id = $_POST['update_id'];
   $update_quantity_query = mysqli_query($conn, "UPDATE `orders` SET status = '$update_value' WHERE id = '$update_id'");
-  if($update_quantity_query){
-     header('location:pending_orders.php');
+  if ($update_quantity_query) {
+    header('location:pending_orders.php');
   };
 };
 
-if(isset($_GET['remove'])){
+if (isset($_GET['remove'])) {
   $remove_id = $_GET['remove'];
   mysqli_query($conn, "DELETE FROM `orders` WHERE id = '$remove_id'");
   header('location:pending_orders.php');
@@ -34,71 +30,80 @@ if(isset($_GET['remove'])){
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="css/pending_orders.css">
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="css/pending_orders.css">
 
 </head>
+
 <body>
 
-<div class="container pendingbody">
-  <h5>Order Status</h5>
-<table class="table">
-  <thead>
-    <tr>
+  <div class="container pendingbody">
+    <h5>Order Status</h5>
+    <table class="table">
+      <thead>
+        <tr>
 
-      <th scope="col">Name</th>
-      <th scope="col">Address</th>
-      <th scope="col">Phone Number</th>
-      <th scope="col">Quantity</th>
-      <th scope="col">Status</th>
-    </tr>
-  </thead>
-  <tbody>
-  <?php
-          if (mysqli_num_rows($result) > 0) {
-            // output data of each row
-            while($row = mysqli_fetch_assoc($result)) {
-              
-              ?>
-    <tr>
+          <th scope="col">Name</th>
+          <th scope="col">Address</th>
+          <th scope="col">Phone</th>
+          <th scope="col">Total Product</th>
+          <th scope="col">Total Price</th>
+          <th scope="col">Status</th>
+        </tr>
+      </thead>
+      <tbody>
+        <?php
+        if (mysqli_num_rows($result) > 0) {
+          // output data of each row
+          while ($row = mysqli_fetch_assoc($result)) {
 
-      <td><?php echo $row["name"] ?></td>
-      <td><?php echo $row["address"] ?></td>
-      <td><?php echo $row["phone"] ?></td>
-      <td><?php echo $row["totalproduct"] ?></td>
-      
-      <td><form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
-        <input type="hidden" name="update_id"  value="<?php echo  $row['id']; ?>" >
-        <div>
-                                <select name="update_status" class="form-control">
-                                <option><?php echo $row['status']; ?></option>
-                                    <option value="Pending">Pending</option>
-                                    <option value="Confirmed">Confirmed</option>
-                                  <option value="Cancel">Cancel</option>
-                                  <option value="Delivered">Delivered</option>
-                                </select>
-                            </div>
-        <input type="submit" value="Update" name="update_update_btn">
-      </form></td>
-      <td>
-        <a href="pending_orders.php?remove=<?php echo $row['id']; ?>">
-        <p>Remove</p>
-      </a></td>
-    </tr>
-    <?php 
-    }
-        } 
-        else 
-            echo "0 results";
         ?>
-        
-  </tbody>
-</table>
-</div>
-    
+            <tr>
+
+              <td><?php echo $row["name"] ?></td>
+              <td><?php echo $row["address"] ?></td>
+              <td><?php echo $row["phone"] ?></td>
+              
+
+              <td><?php echo $row["totalproduct"] ?></td>
+              <td><?php echo $row["totalprice"] ?></td>
+
+              <td>
+                <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+                  <input type="hidden" name="update_id" value="<?php echo  $row['id']; ?>">
+                  <div>
+                    <select name="update_status" class="form-control">
+                      <option><?php echo $row['status']; ?></option>
+                      <option value="Pending">Pending</option>
+                      <option value="Confirmed">Confirmed</option>
+                      <option value="Cancel">Cancel</option>
+                      <option value="Delivered">Delivered</option>
+                    </select>
+                  </div>
+                  <input type="submit" value="Update" name="update_update_btn">
+                </form>
+              </td>
+              <td>
+                <a href="pending_orders.php?remove=<?php echo $row['id']; ?>">
+                  <p>Remove</p>
+                </a>
+              </td>
+            </tr>
+        <?php
+          }
+        } else
+          echo "0 results";
+        ?>
+
+      </tbody>
+    </table>
+  </div>
+
 </body>
+
 </html>
